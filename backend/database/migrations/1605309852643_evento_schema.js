a6e77108ace21b3a0c5b2d8cecae4ab878f2179d
matondo pedro <freitaspedromp@gmail.com>
2020-11-14 22:02:10 +0100

    update table evento

--- backend/database/migrations/1605309852643_evento_schema.js
+++ backend/database/migrations/1605309852643_evento_schema.js
@@ -7,11 +7,8 @@ class EventoSchema extends Schema {
   up () {
     this.table('eventos', (table) => {
       // alter table
-      table.integer('dia').unsigned().notNullable().after('description')
-      table.integer('mes').unsigned().notNullable().after('dia')
-      table.integer('ano').unsigned().notNullable().after('mes')
-      table.integer('hora').unsigned().notNullable().after('ano')
-      table.integer('minuto').unsigned().notNullable().after('hora')
+      table.integer('data').unsigned().notNullable().after('description')
+      table.integer('hora').unsigned().notNullable().after('data')
     })
   }
 
